a2b30bb4a180b487f973e0d5f2b300931d37f500
Daniel-Prus <dlpruszynski@o2.pl>
2022-09-01 15:36:27 +0200

    stg_dim_customers

--- dags/03_incremental_load_scd2_mssql.py
+++ dags/03_incremental_load_scd2_mssql.py
@@ -27,8 +27,9 @@ SOURCE_DB = "NewStoreDB"
 SOURCE_TABLE = "dbo.Orders"
 
 # Target MSSQL database:
-DEST_DB_DWH = "NewStoreDW"
+NEW_STORE_DWH = "NewStoreDW"
 RAW_DATA_DWH_TABLE = "dbo.NewStoreRawData"
+STG_DIM_CUSTOMERS = "dbo.STG_DimCustomers"
 
 "0 10 * * *"
 with DAG("03_incremental_load_scd2_mssql", start_date=datetime(2022, 1, 1),
@@ -47,7 +48,7 @@ with DAG("03_incremental_load_scd2_mssql", start_date=datetime(2022, 1, 1),
     with TaskGroup("process_raw_data") as process_raw_data:
         # etl raw data from mssql NewStoreDB to msql NewStoreDW.dbo.NewStoreRawData by OrderDate
         raw_data_support = MsSqlQuerySupportSCD2(ingest_date=INGEST_DATE, source_db=SOURCE_DB,
-                                                 source_table=SOURCE_TABLE, destination_db=DEST_DB_DWH,
+                                                 source_table=SOURCE_TABLE, destination_db=NEW_STORE_DWH,
                                                  destination_table=RAW_DATA_DWH_TABLE)
         raw_data_query = raw_data_support.load_raw_data_query()
 
@@ -57,7 +58,7 @@ with DAG("03_incremental_load_scd2_mssql", start_date=datetime(2022, 1, 1),
             ingest_date=INGEST_DATE,
             source_db=SOURCE_DB,
             source_table=SOURCE_TABLE,
-            destination_db=DEST_DB_DWH,
+            destination_db=NEW_STORE_DWH,
             destination_table=RAW_DATA_DWH_TABLE,
             sql_query=raw_data_query,
             schedule_interval=INTERVAL,
@@ -65,7 +66,7 @@ with DAG("03_incremental_load_scd2_mssql", start_date=datetime(2022, 1, 1),
         )
 
     with TaskGroup("process_staging") as process_staging:
-        raw_data_sensor_query = MsSqlQuerySupportSCD2(ingest_date=INGEST_DATE, destination_db=DEST_DB_DWH,
+        raw_data_sensor_query = MsSqlQuerySupportSCD2(ingest_date=INGEST_DATE, destination_db=NEW_STORE_DWH,
                                                       destination_table=RAW_DATA_DWH_TABLE) \
             .get_rows_count_sql_sensor_query()
 
@@ -77,21 +78,26 @@ with DAG("03_incremental_load_scd2_mssql", start_date=datetime(2022, 1, 1),
             xcom_task_id_key="NewStoreDW.dbo.NewStoreRawData_rows_affected",
             fail_on_empty=False,
             poke_interval=20,
-            mode="reschedule", #free the worker
+            mode="reschedule",  # free the worker
             timeout=60 * 2,
             soft_fail=False,
             exponential_backoff=True
         )
+        stg_DimCustomers_query = MsSqlQuerySupportSCD2(ingest_date=INGEST_DATE, source_db=NEW_STORE_DWH,
+                                                       source_table=RAW_DATA_DWH_TABLE, destination_db=NEW_STORE_DWH,
+                                                       destination_table=STG_DIM_CUSTOMERS) \
+            .load_staging_dim_customers_query()
 
-        stg_DimCustomers = MsSqlCustomOperator(
-            task_id="stg_DimCustomers",
-            mssql_conn_id="ms_sql_conn",
-            database="NewStoreDW",
-            source_table="dbo.NewStoreRawData",
-            destination_table="dbo.STG_DimCustomers",
-            ingest_date=DAY_1,
+        load_stg_DimCustomers = MsSqlCustomOperator(
+            task_id="load_stg_DimCustomers",
+            conn_id="ms_sql_conn",
+            sql_query=stg_DimCustomers_query,
+            ingest_date=INGEST_DATE,
+            source_db=NEW_STORE_DWH,
+            source_table=RAW_DATA_DWH_TABLE,
+            destination_table=STG_DIM_CUSTOMERS,
+            schedule_interval=INTERVAL,
             autocommit=True
-
         )
 start >> clear_xcom >> Label("etl raw data") >> process_raw_data >> process_staging
 """>> Label("Load staging") >> stg_DimCustomers"""
